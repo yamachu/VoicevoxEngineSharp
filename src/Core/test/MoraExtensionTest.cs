751a7738e261dd34e9b0f16d401694df6de624ea
yamachu <yamachu.dev@gmail.com>
2021-08-18 02:24:20 +0900

    testしたいのでinternalのやつも公開して、テスト追加
    Todo: 他のテストもいい感じにする

---
+++ src/Core/test/MoraExtensionTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Xunit;
+using VoicevoxEngineSharp.Core.Language.Extensions;
+
+namespace VoicevoxEngineSharp.Core.Test
+{
+    public class MoraExtensionTest
+    {
+        [Fact]
+        public void ToTextTest()
+        {
+            // Test case
+            // ref: https://github.com/soimort/python-romkan/blob/c82dc16f1154794fab765cceb2c036d20251717d/tests/test.py#L20-L74
+            Assert.Equal("カンジ", MoraExtensions._ToTextFromJoinedMora("kanji"));
+            Assert.Equal("カンジ", MoraExtensions._ToTextFromJoinedMora("kanzi"));
+            Assert.Equal("カンジ", MoraExtensions._ToTextFromJoinedMora("kannji"));
+            Assert.Equal("チエ", MoraExtensions._ToTextFromJoinedMora("chie"));
+            Assert.Equal("チエ", MoraExtensions._ToTextFromJoinedMora("tie"));
+            Assert.Equal("キョウジュ", MoraExtensions._ToTextFromJoinedMora("kyouju"));
+            Assert.Equal("シュウキョウ", MoraExtensions._ToTextFromJoinedMora("syuukyou"));
+            Assert.Equal("シュウキョウ", MoraExtensions._ToTextFromJoinedMora("shuukyou"));
+            Assert.Equal("サイチュウ", MoraExtensions._ToTextFromJoinedMora("saichuu"));
+            Assert.Equal("サイチュウ", MoraExtensions._ToTextFromJoinedMora("saityuu"));
+            Assert.Equal("チェリー", MoraExtensions._ToTextFromJoinedMora("cheri-"));
+            Assert.Equal("チェリー", MoraExtensions._ToTextFromJoinedMora("tyeri-"));
+            Assert.Equal("シンライ", MoraExtensions._ToTextFromJoinedMora("shinrai"));
+            Assert.Equal("シンライ", MoraExtensions._ToTextFromJoinedMora("sinrai"));
+            Assert.Equal("ハンノウ", MoraExtensions._ToTextFromJoinedMora("hannnou"));
+            Assert.Equal("ハンノウ", MoraExtensions._ToTextFromJoinedMora("han'nou"));
+
+            Assert.Equal("ヲ", MoraExtensions._ToTextFromJoinedMora("wo"));
+            Assert.Equal("ウェ", MoraExtensions._ToTextFromJoinedMora("we"));
+            Assert.Equal("ドゥ", MoraExtensions._ToTextFromJoinedMora("du")); // LibRomkanは "ヅ"
+            Assert.Equal("シェ", MoraExtensions._ToTextFromJoinedMora("she"));
+            Assert.Equal("ディ", MoraExtensions._ToTextFromJoinedMora("di")); // LibRomkanは "ヂ"
+            Assert.Equal("フ", MoraExtensions._ToTextFromJoinedMora("fu"));
+            Assert.Equal("ティ", MoraExtensions._ToTextFromJoinedMora("ti")); // LibRomkanは "チ"
+            Assert.Equal("ウィ", MoraExtensions._ToTextFromJoinedMora("wi"));
+
+            Assert.Equal("ジェ", MoraExtensions._ToTextFromJoinedMora("je"));
+            Assert.Equal("エージェント", MoraExtensions._ToTextFromJoinedMora("e-jento"));
+        }
+    }
+}
---
+++ src/Core/test/PhonemeTest.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using VoicevoxEngineSharp.Core.Language.Models;
+
+namespace VoicevoxEngineSharp.Core.Test
+{
+    public class PhonemeTest
+    {
+        [Fact]
+        public void PhonemeCanParse()
+        {
+            var passLabel = "xx^xx-sil+ch=i/A:xx+xx+xx/B:xx-xx_xx/C:xx_xx+xx/D:07+xx_xx/E:xx_xx!xx_xx-xx/F:xx_xx#xx_xx@xx_xx|xx_xx/G:4_1%0_xx_xx/H:xx_xx/I:xx-xx@xx+xx&xx-xx|xx+xx/J:2_9/K:2+6-23";
+            var phoneme = Phoneme.FromLabel(passLabel);
+            Assert.Equal(passLabel, phoneme.Label);
+        }
+    }
+}
