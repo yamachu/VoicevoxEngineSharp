0f21d9b004a39189055ed7fb4a898341abd3c4ba
yamachu <yamachu.dev@gmail.com>
2021-08-25 20:08:57 +0900

    APIとかから叩くやつをUsecaseにまとめる

---
+++ src/Core/src/Usecases/Synthesis.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using VoicevoxEngineSharp.Core.Acoustic.Models;
+using VoicevoxEngineSharp.Core.Acoustic.Usecases;
+using VoicevoxEngineSharp.Core.Language.Extensions;
+using VoicevoxEngineSharp.Core.Language.Usecases;
+
+namespace VoicevoxEngineSharp.Core.Usecases
+{
+    public class Synthesis
+    {
+        private TextToUtterance _textToUtterance;
+        private SynthesisEngine _synthesisEngine;
+        public Synthesis(TextToUtterance textToUtterance, SynthesisEngine synthesisEngine)
+        {
+            _textToUtterance = textToUtterance;
+            _synthesisEngine = synthesisEngine;
+        }
+
+        public IEnumerable<AccentPhrase> ReplaceMoraPitch(IEnumerable<AccentPhrase> accentPhrases, int speakerId)
+        {
+            return _synthesisEngine.ExtractPhonemeF0(accentPhrases, speakerId);
+        }
+
+        public IEnumerable<AccentPhrase> CreateAccentPhrases(string text, int speakerId)
+        {
+            if (text.Trim() == "")
+            {
+                return new AccentPhrase[] { };
+            }
+            var utterance = _textToUtterance.ToUtterance(text);
+
+            var accentPhrases = utterance.BreathGroups.SelectMany((breathGroup, iBreathGroup) =>
+            {
+                return breathGroup.AccentPhrases.Select((accentPhrase, iAccentPhrase) =>
+                {
+                    return new AccentPhrase
+                    {
+                        Accent = accentPhrase.Accent,
+                        PauseMora = (iAccentPhrase == breathGroup.AccentPhrases.Count() - 1 && iBreathGroup != utterance.BreathGroups.Count() - 1) ? new Mora { Text = "、", Consonant = null, Vowel = "pau", Pitch = 0 } : null,
+                        Moras = accentPhrase.Moras.Select((mora) => new Mora { Text = mora.ToText(), Consonant = mora.Consonant == null ? null : mora.Consonant.GetPhoneme(), Vowel = mora.Vowel.GetPhoneme(), Pitch = 0 })
+                    };
+                });
+            });
+
+            return ReplaceMoraPitch(accentPhrases, speakerId);
+        }
+    }
+}
