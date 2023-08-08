d2de7eb13391fac8dd1f829663866175737f2a6a
yamachu <yamachu.dev@gmail.com>
2023-08-09 00:22:02 +0900

    OpenJtalkもいい感じに
    TODO: テストで使用するDictの場所をもうちょいいい感じに

---
+++ src/Core/src/OpenJtalk.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Runtime.InteropServices;
+using VoicevoxEngineSharp.Core.Enum;
+using VoicevoxEngineSharp.Core.Native;
+using VoicevoxEngineSharp.Core.Struct;
+
+namespace VoicevoxEngineSharp.Core
+{
+    internal class OpenJtalkRcHandle : SafeHandle
+    {
+        public OpenJtalkRcHandle(IntPtr intPtr) : base(IntPtr.Zero, true)
+        {
+            this.SetHandle(intPtr);
+        }
+
+        public override bool IsInvalid => handle == IntPtr.Zero;
+
+        protected override bool ReleaseHandle()
+        {
+            unsafe
+            {
+                CoreUnsafe.voicevox_open_jtalk_rc_delete((OpenJtalkRc*)handle.ToPointer());
+            }
+            return true;
+        }
+
+
+        public static unsafe implicit operator OpenJtalkRc*(OpenJtalkRcHandle handle) => (OpenJtalkRc*)handle.handle.ToPointer();
+    }
+
+    public class OpenJtalk
+    {
+        internal OpenJtalkRcHandle Handle { get; private set; }
+
+        private unsafe OpenJtalk(OpenJtalkRc* rcHandle)
+        {
+            Handle = new OpenJtalkRcHandle(new IntPtr(rcHandle));
+        }
+
+        public static ResultCode New(string openJtalkDicDir, out OpenJtalk openJtalk)
+        {
+            unsafe
+            {
+                var rcHandle = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(OpenJtalkRc)));
+                var p = (OpenJtalkRc*)rcHandle.ToPointer();
+                fixed (byte* ptr = System.Text.Encoding.UTF8.GetBytes(openJtalkDicDir))
+                {
+                    var result = CoreUnsafe.voicevox_open_jtalk_rc_new(ptr, &p).FromNative();
+                    openJtalk = new OpenJtalk(p);
+
+                    return result;
+                }
+            }
+        }
+
+        public ResultCode Import(UserDict userDict)
+        {
+            unsafe
+            {
+                return CoreUnsafe.voicevox_open_jtalk_rc_use_user_dict((OpenJtalkRc*)Handle, (VoicevoxUserDict*)userDict.Handle).FromNative();
+            }
+        }
+    }
+}
---
+++ src/Core/test/OpenJtalk.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using VoicevoxEngineSharp.Core.Enum;
+using VoicevoxEngineSharp.Core.Struct;
+using Xunit;
+
+namespace VoicevoxEngineSharp.Core.Test
+{
+    public class OpenJtalkTest
+    {
+        [Fact]
+        public void Open()
+        {
+            var openResult = OpenJtalk.New(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads", "open_jtalk_dic_utf_8-1.11"), out var openJtalk);
+            Assert.Equal(ResultCode.RESULT_OK, openResult);
+            Assert.NotNull(openJtalk);
+        }
+
+        [Fact]
+        public void UseUserDict()
+        {
+            OpenJtalk.New(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads", "open_jtalk_dic_utf_8-1.11"), out var openJtalk);
+            // 空っぽの辞書をコンパイルしようとするとクラッシュするので足す
+            var userDict = new UserDict();
+            userDict.AddWord(UserDictWord.Create("hoge", "ホゲ"), out var _);
+            var result = openJtalk.Import(userDict);
+            Assert.Equal(ResultCode.RESULT_OK, result);
+        }
+    }
+}
